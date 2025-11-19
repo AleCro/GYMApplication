92b45a00316d8ea6fd212849ca0563a80e57886c
AleCro <you@example.com>
2025-11-19 20:04:59 +0000

    vercel

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,14 +1,14 @@
-// import adapter from "svelte-adapter-bun";
+import adapter from "svelte-adapter-bun";
 // import adapter from '@sveltejs/adapter-node';
 import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// adapter: adapter()
-		adapter: adapter({
-  			runtime: 'nodejs20.x'
-		})
+		adapter: adapter()
+		// adapter: adapter({
+  		// 	runtime: 'nodejs20.x'
+		// })
 	}
 };
 
